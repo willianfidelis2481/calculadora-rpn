dd74cc8050fa9634e6d7ee329a55ba7016872155
willianfidelis2481 <willianfernandofidelis@gmail.com>
2026-09-12 20:47:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'prata-rpn-v9';
+const CACHE = 'prata-rpn-v10';
 const ASSETS = ['index.html', 'manifest.json', 'icon.svg', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
